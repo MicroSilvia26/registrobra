5fb4e8106c30ca171a42e861caa5d36d5c71cd13
BQS <microbiologia@bqscolombia.com>
2026-09-23 15:06:29 -0500

    Update apiUrl in config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
    Pegue entre las comillas el enlace de la "Aplicación web" de Google Apps Script
    (termina en /exec). Ejemplo: 'https://script.google.com/macros/s/AKfy.../exec' */
 window.APP_CONFIG = {
-  apiUrl: ''
+  apiUrl: 'https://script.google.com/macros/s/AKfycbxPnZwGy4W1C29cvTQxin2rm_pNDGSHUACfaWGz5DN0pI86HfEb6DMhYQ-eK6b_yebj/exec'
 };
